7880b7043bec1290e11b19acb00d42492f3f3448
1378930391 <1378930391@qq.com>
2019-03-27 13:19:12 +0800

    框架提交

---
+++ src/main/java/action/HelloAction.java
@@ -0,0 +1,10 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+    public void hello(){
+        System.out.println("success");
+    }
+}
