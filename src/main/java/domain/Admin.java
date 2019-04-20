8e7b8ad89468aa02a2e23fac4e420b8d19c0a76e
1378930391 <1378930391@qq.com>
2019-04-20 11:36:26 +0800

    添加管理员

---
+++ src/main/java/domain/Admin.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Admin {
+    private Integer admin_id;
+    private Integer admin_name;
+    private String admin_password;
+
+    public Integer getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(Integer admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public Integer getAdmin_name() {
+        return admin_name;
+    }
+
+    public void setAdmin_name(Integer admin_name) {
+        this.admin_name = admin_name;
+    }
+
+    public String getAdmin_password() {
+        return admin_password;
+    }
+
+    public void setAdmin_password(String admin_password) {
+        this.admin_password = admin_password;
+    }
+}
